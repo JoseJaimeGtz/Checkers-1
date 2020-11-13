98edffb5011d707740106692a1e7e81361eff71c
rodrigo-zamora <is727254@iteso.mx>
2020-11-12 20:53:10 -0600

    fixed board height

--- game.c
+++ game.c
@@ -68,58 +68,58 @@ void createWindow(gameStructRef game)
         ClearBackground(RAYWHITE);
 
         if(game->boardsize == 8) DrawRectangleLines(300, 80, 640, 640, BLACK);
-        else if(game->boardsize == 10) DrawRectangleLines(300, 80, 800, 800, BLACK);
-        else if(game->boardsize == 12)DrawRectangleLines(300, 80, 960, 960, BLACK);
+        if(game->boardsize == 10) DrawRectangleLines(300, 80, 800, 800, BLACK);
+        if(game->boardsize == 12)DrawRectangleLines(300, 80, 960, 960, BLACK);
 
         for(int i = 0; i < game->boardsize; i++){
             if (i%2 != 0){
-                DrawRectangle(300, 80+(i*80), 80, 80, BROWN);
-                DrawRectangle(460, 80+(i*80), 80, 80, BROWN);
-                DrawRectangle(620, 80+(i*80), 80, 80, BROWN);
-                DrawRectangle(780, 80+(i*80), 80, 80, BROWN);
-                if(game->boardsize >= 10) DrawRectangle(940, 80+(i*80), 80, 80, BROWN);
-                if(game->boardsize == 12) DrawRectangle(1100, 80+(i*80), 80, 80, BROWN);
+                DrawRectangle(300, 40+(i*80), 80, 80, BROWN);
+                DrawRectangle(460, 40+(i*80), 80, 80, BROWN);
+                DrawRectangle(620, 40+(i*80), 80, 80, BROWN);
+                DrawRectangle(780, 40+(i*80), 80, 80, BROWN);
+                if(game->boardsize >= 10) DrawRectangle(940, 40+(i*80), 80, 80, BROWN);
+                if(game->boardsize == 12) DrawRectangle(1100, 40+(i*80), 80, 80, BROWN);
             } else {
-                DrawRectangle(380, 80+(i*80), 80, 80, BROWN);
-                DrawRectangle(540, 80+(i*80), 80, 80, BROWN);
-                DrawRectangle(700, 80+(i*80), 80, 80, BROWN);
-                DrawRectangle(860, 80+(i*80), 80, 80, BROWN);
-                if(game->boardsize >= 10) DrawRectangle(1020, 80+(i*80), 80, 80, BROWN);
-                if(game->boardsize == 12) DrawRectangle(1180, 80+(i*80), 80, 80, BROWN);
+                DrawRectangle(380, 40+(i*80), 80, 80, BROWN);
+                DrawRectangle(540, 40+(i*80), 80, 80, BROWN);
+                DrawRectangle(700, 40+(i*80), 80, 80, BROWN);
+                DrawRectangle(860, 40+(i*80), 80, 80, BROWN);
+                if(game->boardsize >= 10) DrawRectangle(1020, 40+(i*80), 80, 80, BROWN);
+                if(game->boardsize == 12) DrawRectangle(1180, 40+(i*80), 80, 80, BROWN);
             }
         }
         for(int i = 0; i < game->boardsize; i++){
             if(i <= 2) {
                 if(i == 1) {
-                    DrawCircle(340, 120+(i*80), 30, WHITE);
-                    DrawCircle(500, 120+(i*80), 30, WHITE);
-                    DrawCircle(660, 120+(i*80), 30, WHITE);
-                    DrawCircle(820, 120+(i*80), 30, WHITE);
-                    if(game->boardsize >= 10) DrawCircle(980, 120+(i*80), 30, WHITE);
-                    if(game->boardsize == 12) DrawCircle(1140, 120+(i*80), 30, WHITE);
+                    DrawCircle(340, 80+(i*80), 30, WHITE);
+                    DrawCircle(500, 80+(i*80), 30, WHITE);
+                    DrawCircle(660, 80+(i*80), 30, WHITE);
+                    DrawCircle(820, 80+(i*80), 30, WHITE);
+                    if(game->boardsize >= 10) DrawCircle(980, 80+(i*80), 30, WHITE);
+                    if(game->boardsize == 12) DrawCircle(1140, 80+(i*80), 30, WHITE);
                 } else {
-                    DrawCircle(420, 120+(i*80), 30, WHITE);
-                    DrawCircle(580, 120+(i*80), 30, WHITE);
-                    DrawCircle(740, 120+(i*80), 30, WHITE);
-                    DrawCircle(900, 120+(i*80), 30, WHITE);
-                    if(game->boardsize >= 10) DrawCircle(1060, 120+(i*80), 30, WHITE);
-                    if(game->boardsize == 12) DrawCircle(1220, 120+(i*80), 30, WHITE);
+                    DrawCircle(420, 80+(i*80), 30, WHITE);
+                    DrawCircle(580, 80+(i*80), 30, WHITE);
+                    DrawCircle(740, 80+(i*80), 30, WHITE);
+                    DrawCircle(900, 80+(i*80), 30, WHITE);
+                    if(game->boardsize >= 10) DrawCircle(1060, 80+(i*80), 30, WHITE);
+                    if(game->boardsize == 12) DrawCircle(1220, 80+(i*80), 30, WHITE);
                 }
             } else if(i >= 5) {
                 if(i == 6) {
-                    DrawCircle(420, 120+(i*80), 30, BLACK);
-                    DrawCircle(580, 120+(i*80), 30, BLACK);
-                    DrawCircle(740, 120+(i*80), 30, BLACK);
-                    DrawCircle(900, 120+(i*80), 30, BLACK);
-                    if(game->boardsize >= 10) DrawCircle(1060, 120+(i*80), 30, WHITE);
-                    if(game->boardsize == 12) DrawCircle(1220, 120+(i*80), 30, WHITE);
+                    DrawCircle(420, 80+(i*80), 30, BLACK);
+                    DrawCircle(580, 80+(i*80), 30, BLACK);
+                    DrawCircle(740, 80+(i*80), 30, BLACK);
+                    DrawCircle(900, 80+(i*80), 30, BLACK);
+                    if(game->boardsize >= 10) DrawCircle(1060, 80+(i*80), 30, WHITE);
+                    if(game->boardsize == 12) DrawCircle(1220, 80+(i*80), 30, WHITE);
                 } else {
-                    DrawCircle(340, 120+(i*80), 30, BLACK);
-                    DrawCircle(500, 120+(i*80), 30, BLACK);
-                    DrawCircle(660, 120+(i*80), 30, BLACK);
-                    DrawCircle(820, 120+(i*80), 30, BLACK);
-                    if(game->boardsize >= 10) DrawCircle(980, 120+(i*80), 30, WHITE);
-                    if(game->boardsize == 12) DrawCircle(1140, 120+(i*80), 30, WHITE);
+                    DrawCircle(340, 80+(i*80), 30, BLACK);
+                    DrawCircle(500, 80+(i*80), 30, BLACK);
+                    DrawCircle(660, 80+(i*80), 30, BLACK);
+                    DrawCircle(820, 80+(i*80), 30, BLACK);
+                    if(game->boardsize >= 10) DrawCircle(980, 80+(i*80), 30, WHITE);
+                    if(game->boardsize == 12) DrawCircle(1140, 80+(i*80), 30, WHITE);
                 }
             }
         }
