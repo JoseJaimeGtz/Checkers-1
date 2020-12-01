8a2db1f2762478b6db31df056c9b23836bbcd38b
Jose Jaime Gtz <jose_jaime@live.com.mx>
2020-12-01 17:00:46 -0600

    BlackPiecesColor

--- graphics.h
+++ graphics.h
@@ -14,7 +14,7 @@ void checkLoadButton(gameStructRef game, mainButtonsStruct board, ScreenFlag *sc
 
 // COLORS
 #define BOARD (Color){ 166, 98, 0, 255 }
-#define BLACKPIECES (Color){40, 40, 40, 255}
+#define BLACKPIECES (Color){60, 60, 60, 255}
 #define WHITEPIECES (Color){239, 241, 243, 195}
 #define BLACKPIECESKING (Color){1, 1, 1, 255}
 #define WHITEPIECESKING (Color){255, 255, 255, 255}
