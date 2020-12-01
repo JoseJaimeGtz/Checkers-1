4fb1bcfe38999f435bf7943bed61605b6edfec9c
Jose Jaime Gtz <jose_jaime@live.com.mx>
2020-12-01 14:57:01 -0600

    Board 10x10 fixed

--- game.c
+++ game.c
@@ -23,22 +23,25 @@ void createBoard(gameStructRef game)
     for(int y = 1; y <= game->boardsize; y++){
         for(int x = 1; x <= game->boardsize; x++){
             if (y<=game->boardsize/2-1){ // Fichas blancas
-                if((y%2!=0 && x%2==0) || (y%2==0 && x%2!=0)){         
+                if((y%2!=0 && x%2==0) || (y%2==0 && x%2!=0)){
+                    //fprintf(stderr, "Ficha blanca [%d][%d]\n", x, y);
                     pieceStructRef new = newPiece(game, 2, 1);
                     game->board[x][y] = new;
                 }
             } else if (y>=game->boardsize/2+2){ // Fichas Negras
                 if((y%2!=0 && x%2==0) || y%2==0 && x%2!=0){
+                    //fprintf(stderr, "Ficha negra [%d][%d]\n", x, y);
                     pieceStructRef new = newPiece(game, 1, 1);
                     game->board[x][y] = new;
                 }
-            } else if ((y == game->boardsize/2 && (y%2==0 && x%2!=0)) || (y == game->boardsize/2+1 && (y%2!=0 && x%2==0))){
+            } else if ((y == game->boardsize/2 && ((y%2==0 && x%2!=0) || (y%2!=0 && x%2==0))) || (y == game->boardsize/2+1 && ((y%2!=0 && x%2==0) || (y%2==0 && x%2!=0)))){
                 //fprintf(stderr, "posición vacia [%d][%d]\n", x, y);
                 pieceStructRef new = newPiece(game, 0, 0);
                 game->board[x][y] = new;
             }
         }
     }
+    fprintf(stderr, "Tablero creado\n");
 }
 
 void saveGame(gameStructRef game, int slot)
